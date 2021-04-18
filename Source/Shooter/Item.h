baa9ba4d17157a5c4b1f02800298ed6d42d6457e
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-04-18 10:53:54 -0700

    Binding the Item Icon

--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -232,6 +232,10 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Inventory, meta = (AllowPrivateAccess = "true"))
 	UTexture2D* IconBackground;
 
+	/** Icon for this item in the inventory */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Inventory, meta = (AllowPrivateAccess = "true"))
+	UTexture2D* IconItem;
+
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const { return PickupWidget; }
 	FORCEINLINE USphereComponent* GetAreaSphere() const { return AreaSphere; }
