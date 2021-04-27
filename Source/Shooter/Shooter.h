3318451476507fab6ab57cbf4a9570caadab9e17
DruidMech <45446093+DruidMech@users.noreply.github.com>
2021-04-27 08:31:10 -0600

    Surface Types in C++

--- Source/Shooter/Shooter.h
+++ Source/Shooter/Shooter.h
@@ -4,3 +4,8 @@
 
 #include "CoreMinimal.h"
 
+#define EPS_Metal EPhysicalSurface::SurfaceType1
+#define EPS_Stone EPhysicalSurface::SurfaceType2
+#define EPS_Tile EPhysicalSurface::SurfaceType3
+#define EPS_Grass EPhysicalSurface::SurfaceType4
+#define EPS_Water EPhysicalSurface::SurfaceType5
