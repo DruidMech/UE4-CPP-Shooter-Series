f45d333af00eb3dae0faecd570fcdb4ece888a91
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-06-21 13:05:18 -0600

    Add files via upload

---
+++ Code Per Lesson/101 Reload Continued/Weapon.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "AmmoType.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	EWT_SubmachineGun UMETA(DisplayName = "SubmachineGun"),
+	EWT_AssaultRifle UMETA(DisplayName = "AssaultRifle"),
+
+	EWT_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+public:
+	AWeapon();
+
+	virtual void Tick(float DeltaTime) override;
+protected:
+	void StopFalling();
+private:
+	FTimerHandle ThrowWeaponTimer;
+	float ThrowWeaponTime;
+	bool bFalling;
+
+	/** Ammo count for this Weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	int32 Ammo;
+
+	/** The type of weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	EWeaponType WeaponType;
+
+	/** The type of ammo for this weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	EAmmoType AmmoType;
+
+	/** FName for the Reload Montage Section */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	FName ReloadMontageSection;
+
+public:
+	/** Adds an impulse to the Weapon */
+	void ThrowWeapon();
+
+	FORCEINLINE int32 GetAmmo() const { return Ammo; }
+
+	/** Called from Character class when firing Weapon */
+	void DecrementAmmo();
+
+	FORCEINLINE EWeaponType GetWeaponType() const { return WeaponType; }
+	FORCEINLINE EAmmoType GetAmmoType() const { return AmmoType; }
+	FORCEINLINE FName GetReloadMontageSection() const { return ReloadMontageSection; }
+};
---
+++ Code Per Lesson/102 Update AmmoMap/Weapon.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+
+AWeapon::AWeapon() :
+	ThrowWeaponTime(0.7f),
+	bFalling(false),
+	Ammo(30),
+	MagazineCapacity(30),
+	WeaponType(EWeaponType::EWT_SubmachineGun),
+	AmmoType(EAmmoType::EAT_9mm),
+	ReloadMontageSection(FName(TEXT("Reload SMG")))
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Keep the Weapon upright
+	if (GetItemState() == EItemState::EIS_Falling && bFalling)
+	{
+		const FRotator MeshRotation{ 0.f, GetItemMesh()->GetComponentRotation().Yaw, 0.f };
+		GetItemMesh()->SetWorldRotation(MeshRotation, false, nullptr, ETeleportType::TeleportPhysics);
+	}
+}
+
+void AWeapon::ThrowWeapon()
+{
+	FRotator MeshRotation{ 0.f, GetItemMesh()->GetComponentRotation().Yaw, 0.f };
+	GetItemMesh()->SetWorldRotation(MeshRotation, false, nullptr, ETeleportType::TeleportPhysics);
+
+	const FVector MeshForward{ GetItemMesh()->GetForwardVector() };
+	const FVector MeshRight{ GetItemMesh()->GetRightVector() };
+	// Direction in which we throw the Weapon
+	FVector ImpulseDirection = MeshRight.RotateAngleAxis(-20.f, MeshForward);
+
+	float RandomRotation{ 30.f };
+	ImpulseDirection = ImpulseDirection.RotateAngleAxis(RandomRotation, FVector(0.f, 0.f, 1.f));
+	ImpulseDirection *= 20'000.f;
+	GetItemMesh()->AddImpulse(ImpulseDirection);
+
+	bFalling = true;
+	GetWorldTimerManager().SetTimer(
+		ThrowWeaponTimer, 
+		this, 
+		&AWeapon::StopFalling, 
+		ThrowWeaponTime);
+
+}
+
+void AWeapon::StopFalling()
+{
+	bFalling = false;
+	SetItemState(EItemState::EIS_Pickup);
+}
+
+void AWeapon::DecrementAmmo()
+{
+	if (Ammo - 1 <= 0)
+	{
+		Ammo = 0;
+	}
+	else
+	{
+		--Ammo;
+	}
+}
+
+void AWeapon::ReloadAmmo(int32 Amount)
+{
+	checkf(Ammo + Amount <= MagazineCapacity, TEXT("Attempted to reload with more than magazine capacity!"));
+	Ammo += Amount;
+}
