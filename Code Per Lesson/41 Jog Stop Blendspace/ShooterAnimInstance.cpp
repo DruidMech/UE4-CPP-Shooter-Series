c4a9d47b5d43d864c929b9ce4b3151805af26f5c
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-06-21 12:54:52 -0600

    Add files via upload

---
+++ Code Per Lesson/41 Jog Stop Blendspace/ShooterAnimInstance.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterAnimInstance.h"
+#include "ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+	if (ShooterCharacter)
+	{
+		// Get the lateral speed of the character from velocity
+		FVector Velocity{ ShooterCharacter->GetVelocity() };
+		Velocity.Z = 0;
+		Speed = Velocity.Size();
+
+		// Is the character in the air?
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+		// Is the character accelerating?
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f)
+		{
+			bIsAccelerating = true;
+		}
+		else
+		{
+			bIsAccelerating = false;
+		}
+
+		FRotator AimRotation = ShooterCharacter->GetBaseAimRotation();
+		FRotator MovementRotation =
+			UKismetMathLibrary::MakeRotFromX(
+				ShooterCharacter->GetVelocity());
+		MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(
+			MovementRotation,
+			AimRotation).Yaw;
+
+		
+		if (ShooterCharacter->GetVelocity().Size() > 0.f)
+		{
+			LastMovementOffsetYaw = MovementOffsetYaw;
+		}
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
