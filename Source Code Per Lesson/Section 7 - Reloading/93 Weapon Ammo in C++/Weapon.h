947d0a291b002ea80af02688f17ac77e0605e879
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-07-11 11:38:28 -0600

    Add files via upload

---
+++ Source Code Per Lesson/Section 7 - Reloading/93 Weapon Ammo in C++/Weapon.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+public:
+	AWeapon();
+
+	virtual void Tick(float DeltaTime) override;
+protected:
+	void StopFalling();
+private:
+	FTimerHandle ThrowWeaponTimer;
+	float ThrowWeaponTime;
+	bool bFalling;
+
+	/** Ammo count for this Weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	int32 Ammo;
+
+public:
+	/** Adds an impulse to the Weapon */
+	void ThrowWeapon();
+
+	FORCEINLINE int32 GetAmmo() const { return Ammo; }
+
+	/** Called from Character class when firing Weapon */
+	void DecrementAmmo();
+};
