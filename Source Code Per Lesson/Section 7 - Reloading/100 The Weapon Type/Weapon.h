addf9ab6cf0ffd9d3515b2d701f68f834b532f80
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-07-11 11:39:32 -0600

    Add files via upload

---
+++ Source Code Per Lesson/Section 7 - Reloading/100 The Weapon Type/Weapon.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	EWT_SubmachineGun UMETA(DisplayName = "SubmachineGun"),
+	EWT_AssaultRifle UMETA(DisplayName = "AssaultRifle"),
+	EWT_Pistol UMETA(DisplayName = "Pistol"),
+
+	EWT_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+public:
+	AWeapon();
+
+	virtual void Tick(float DeltaTime) override;
+protected:
+	void StopFalling();
+private:
+	FTimerHandle ThrowWeaponTimer;
+	float ThrowWeaponTime;
+	bool bFalling;
+
+	/** Ammo count for this Weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	int32 Ammo;
+	
+	/** The type of weapon */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	EWeaponType WeaponType;
+
+public:
+	/** Adds an impulse to the Weapon */
+	void ThrowWeapon();
+
+	FORCEINLINE int32 GetAmmo() const { return Ammo; }
+
+	/** Called from Character class when firing Weapon */
+	void DecrementAmmo();
+	
+	FORCEINLINE EWeaponType GetWeaponType() const { return WeaponType; }
+};
