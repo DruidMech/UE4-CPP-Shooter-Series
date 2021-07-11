d647e3623d71b536c3826e1fd6f8bd7e6c9df345
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-07-11 11:30:26 -0600

    Add files via upload

---
+++ Source Code Per Lesson/Section 2 - Project Setup/09 UE_LOG Format String - Int/ShooterCharacter.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay() called!"));
+
+	int myInt{ 42 };
+	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
