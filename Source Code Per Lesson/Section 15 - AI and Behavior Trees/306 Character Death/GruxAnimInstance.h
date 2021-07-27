374532d069f81cc650cea556178a8d80f149dd4a
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-07-26 21:04:07 -0600

    Add files via upload

---
+++ Source Code Per Lesson/Section 15 - AI and Behavior Trees/306 Character Death/GruxAnimInstance.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GruxAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UGruxAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+private:
+	/** Lateral Movement speed */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class AEnemy* Enemy;
+	
+};
