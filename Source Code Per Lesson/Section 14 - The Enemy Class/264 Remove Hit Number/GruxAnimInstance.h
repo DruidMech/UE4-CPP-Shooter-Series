8c0a238b59484b989050b96fb3945c71764104f9
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-07-26 20:54:20 -0600

    Add files via upload

---
+++ Source Code Per Lesson/Section 14 - The Enemy Class/264 Remove Hit Number/GruxAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GruxAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UGruxAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
