4eb0fb3989a934ec4709b5eb3f364bcb8911cc03
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-07-26 21:11:38 -0600

    Add files via upload

---
+++ Source Code Per Lesson/Section 16 - Khaimera/318 Explosive Get Overlapping Actors/Explosive.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BulletHitInterface.h"
+#include "Explosive.generated.h"
+
+UCLASS()
+class SHOOTER_API AExplosive : public AActor, public IBulletHitInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AExplosive();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	/** Explosion when hit by a bullet */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class UParticleSystem* ExplodeParticles;
+
+	/** Sound to play when hit by bullets */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class USoundCue* ImpactSound;
+
+	/** Mesh for the explosive */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* ExplosiveMesh;
+
+	/** Used to determine what Actors overlap during explosion */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* OverlapSphere;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BulletHit_Implementation(FHitResult HitResult, AActor* Shooter) override;
+};
