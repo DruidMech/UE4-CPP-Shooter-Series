57a19f9cabcb8b01534903a56ffcde7895cbd915
Stephen Ulibarri <45446093+DruidMech@users.noreply.github.com>
2021-07-26 20:58:12 -0600

    Add files via upload

---
+++ Source Code Per Lesson/Section 15 - AI and Behavior Trees/284 In Attack Range/Explosive.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Explosive.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+AExplosive::AExplosive()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AExplosive::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AExplosive::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AExplosive::BulletHit_Implemention(FHitResult HitResult)
+{
+	if (ImpactSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation());
+	}
+	if (ExplodeParticles)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplodeParticles, HitResult.Location, FRotator(0.f), true);
+	}
+
+	// TODO: Apply explosive damage
+
+	Destroy();
+
+}
+
